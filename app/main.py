1b668e06df14b88948f9cb3507bba16c9017aa81
Zahar <galushkozahar>
2025-03-15 14:53:37 +0200

    Виправив

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def copy_file(commands: str) -> None:
             course_file = parts[1]
             dei_file = parts[2]
             with (open(course_file, "r") as file1,
-                  open(dei_file, "w+") as file2):
+                  open(dei_file, "w") as file2):
                 for line in file1:
                     file2.write(line)
         except FileNotFoundError:
