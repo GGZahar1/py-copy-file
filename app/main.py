43f543fe9b28f47149ddea4720ffb5592375cb46
Zahar <galushkozahar>
2025-03-15 14:51:24 +0200

    Виконав

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(commands: str) -> None:
+    parts = commands.split(" ")
+    if len(parts) == 3 and parts[0] == "cp":
+        try:
+            course_file = parts[1]
+            dei_file = parts[2]
+            with (open(course_file, "r") as file1,
+                  open(dei_file, "w+") as file2):
+                for line in file1:
+                    file2.write(line)
+        except FileNotFoundError:
+            return
+    else:
+        return
